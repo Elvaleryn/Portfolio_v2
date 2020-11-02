81a49133afc466a3105ba290155a180eea3d8019
eray <eraykaya121@gmail.com>
2020-11-02 21:59:22 +0300

    home logo nav highlighted now

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
 import Burger from './Burger';
+import { useRouter } from 'next/router';
+
 const Navbar = () => {
+	const router = useRouter();
+    console.log(router.pathname);
 	return (
 		<nav className="container flex justify-between items-center mx-auto mt-4 sm:px-4">
 			<span className="home">
@@ -14,17 +18,24 @@ const Navbar = () => {
 			<Burger />
 
 			<style jsx>{`
+				.highlighted-logo {
+					@apply border border-solid border-white p-2;
+				}
+
+				.highlighted-logo {
+					background-color: ${router.pathname === '/' ? 'white' : ''};
+					color: ${router.pathname === '/' ? 'black' : ''};
+				}
 
 				.home {
 					@apply mr-auto;
 				}
 
-                @media (max-width: 768px) {
-                    nav {
-                        @apply px-4;
-                    }
-                }
-
+				@media (max-width: 768px) {
+					nav {
+						@apply px-4;
+					}
+				}
 			`}</style>
 		</nav>
 	);
